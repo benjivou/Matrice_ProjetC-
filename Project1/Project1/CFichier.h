db0d6d34a25f00a387dd098cbc07f1390e3822c0
benjivou <benjivou2@gmail.com>
2019-04-20 19:35:14 +0200

    Création d'une branche de travail Cfichier

--- Project1/Project1/CFichier.h
+++ Project1/Project1/CFichier.h
@@ -1,7 +1,12 @@
+/*	@brief This class permit to catch the matrice from a 
+*	.txt fich to a object Matrice
+*/
+
 class CFichier
 {
 public:
 	CFichier();
+	CFichier(char* adr);
 	~CFichier();
 
 private:
