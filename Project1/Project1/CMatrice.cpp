c55c80f1cf0ab86318ccc0e3ce4078bc05d556f1
benjivou <benjivou2@gmail.com>
2019-04-24 19:59:24 +0200

    Version fonctionnelle

---
+++ Project1/Project1/CMatrice.cpp
@@ -0,0 +1,139 @@
+// CMatrice.cpp : fichier source correspondant à l'en-tête précompilé ; nécessaire au bon déroulement de la compilation
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Constructeurs et destructeurs
+
+template<class MType>
+CMatrice<MType>::CMatrice()
+{
+	uiMTPNbColonne = 0;
+	uiMTPNbLigne = 0;
+	ppMTPMatrice = nullptr;
+}
+
+template<class MType>
+CMatrice<MType>::CMatrice(unsigned int uiTaille)
+{
+	uiMTPNbColonne = uiTaille;
+	uiMTPNbLigne = uiTaille;
+
+	ppMTPMatrice = new MType*[uiMTPNbLigne];
+
+	for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+	{
+		ppMTPMatrice[uiMTPBoucle] = new MType[uiMTPNbColonne];
+	}
+
+	for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+	{
+		for (unsigned int uiMTPBoucle1 = 0; uiMTPBoucle1 < uiMTPNbColonne; uiMTPBoucle1++)
+		{
+			ppMTPMatrice[uiMTPBoucle][uiMTPBoucle1] = 0;
+		}
+	}
+}
+
+template<class MType>
+CMatrice<MType>::CMatrice(unsigned int uiNbLigne, unsigned int uiNBCol)
+{
+	uiMTPNbColonne = uiNBCol;
+	uiMTPNbLigne = uiNbLigne;
+
+	ppMTPMatrice = new MType*[uiMTPNbLigne];
+
+	for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+	{
+		ppMTPMatrice[uiMTPBoucle] = new MType[uiMTPNbColonne];
+	}
+
+	for (unsigned int uiMTPBoucle1 = 0; uiMTPBoucle1 < uiMTPNbLigne; uiMTPBoucle1++)
+	{
+		for (unsigned int uiMTPBoucle2 = 0; uiMTPBoucle2 < uiMTPNbColonne; uiMTPBoucle2++)
+		{
+			ppMTPMatrice[uiMTPBoucle1][uiMTPBoucle2] = 0;
+		}
+	}
+}
+
+template<class MType>
+CMatrice<MType>::CMatrice(CMatrice<MType>& MTPParam)
+{
+	uiMTPNbColonne = MTPParam.uiMTPNbColonne;
+	uiMTPNbLigne = MTPParam.uiMTPNbLigne;
+
+	ppMTPMatrice = new MType*[uiMTPNbLigne];
+
+	for (unsigned int uiMTPBoucle2 = 0; uiMTPBoucle2 < uiMTPNbLigne; uiMTPBoucle2++)
+	{
+		ppMTPMatrice[uiMTPBoucle2] = new MType[uiMTPNbColonne];
+	}
+
+	for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+	{
+		for (unsigned int uiMTPBoucle1 = 0; uiMTPBoucle1 < uiMTPNbColonne; uiMTPBoucle1++)
+		{
+			ppMTPMatrice[uiMTPBoucle][uiMTPBoucle1] = MTPParam.ppMTPMatrice[uiMTPBoucle][uiMTPBoucle1];
+		}
+	}
+}
+
+template<class MType>
+CMatrice<MType>::~CMatrice()
+{
+	//Ne fais rien
+}
+
+// Méthodes
+
+template<class MType>
+CMatrice<MType> & CMatrice<MType>::operator=(CMatrice<MType> &MTPParam)
+{
+	uiMTPNbColonne = MTPParam.uiMTPNbColonne;
+	uiMTPNbLigne = MTPParam.uiMTPNbLigne;
+
+	ppMTPMatrice = new MType*[uiMTPNbLigne];
+
+	for (unsigned int uiMTPBoucle2 = 0; uiMTPBoucle2 < uiMTPNbLigne; uiMTPBoucle2++)
+	{
+		ppMTPMatrice[uiMTPBoucle2] = new MType[uiMTPNbColonne];
+	}
+
+	for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+	{
+		for (unsigned int uiMTPBoucle1 = 0; uiMTPBoucle1 < uiMTPNbColonne; uiMTPBoucle1++)
+		{
+			ppMTPMatrice[uiMTPBoucle][uiMTPBoucle1] = MTPParam.ppMTPMatrice[uiMTPBoucle][uiMTPBoucle1];
+		}
+	}
+	return *this;
+}
+
+template<class MType>
+void CMatrice<MType>::MTPAfficherMatrice()
+{
+	printf("Matrice : \n");
+	for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+	{
+		for (unsigned int uiMTPBoucle1 = 0; uiMTPBoucle1 < uiMTPNbColonne; uiMTPBoucle1++)
+		{
+			printf(" %d ", ppMTPMatrice[uiMTPBoucle][uiMTPBoucle1]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+template<class MType>
+void CMatrice<MType>::MTPViderMatrice()
+{
+	uiMTPNbColonne = 0;
+	uiMTPNbLigne = 0;
+	if (ppMTPMatrice)
+	{
+		for (unsigned int uiMTPBoucle = 0; uiMTPBoucle < uiMTPNbLigne; uiMTPBoucle++)
+			delete[] ppMTPMatrice[uiMTPBoucle];
+		delete[] ppMTPMatrice;
+	}
+}
---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,37 @@
+// main.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
+#include "CMatrice.h"
+
+#include <iostream>
+
+
+int main()
+{
+	printf("Hello World!\n");
+
+	CMatrice <int> MatriceEntier1(3, 2);
+	CMatrice <int> MatriceEntier2(MatriceEntier1);
+
+	for (unsigned int i = 0; i < MatriceEntier1.MTPLire_NbLigne(); i++)
+	{
+		for (unsigned int j = 0; j < MatriceEntier1.MTPLire_NbColonne(); j++)
+		{
+			MatriceEntier1.MTPModifier_Element(i, j, i);
+		}
+	}
+
+	MatriceEntier1.MTPAfficherMatrice();
+	MatriceEntier2.MTPAfficherMatrice();
+
+	MatriceEntier1.MTPViderMatrice();
+	MatriceEntier2.MTPViderMatrice();
+
+	//printf("\n %d", MatriceEntier1.MTPLire_NbColonne());
+
+	getchar();
+	return 0;
+}
+
+
