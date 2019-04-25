ec892d1e8402b1be633b9a838f88dba2239c83d9
benjivou <benjivou2@gmail.com>
2019-04-25 09:54:23 +0200

    v1

---
+++ Project1/Project1/CFichier.h
@@ -0,0 +1,15 @@
+/*	@brief This class permit to catch the matrice from a 
+*	.txt fich to a object Matrice
+*/
+
+class CFichier
+{
+public:
+	CFichier();
+	CFichier(char* adr);
+	~CFichier();
+
+private:
+
+};
+
